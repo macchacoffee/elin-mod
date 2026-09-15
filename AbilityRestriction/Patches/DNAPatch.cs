c33394a9be2ff468b7ee91321d7fd56bc962672b
macchacoffee <231792543+macchacoffee@users.noreply.github.com>
2026-09-15 21:13:19 +0900

    Ability Restriction 遺伝子削除で制限中のアビリティを消去した後、遺伝子追加での同じアビリティ再習得が重複チェックで拒否されていたのを修正

--- AbilityRestriction/Patches/DNAPatch.cs
+++ AbilityRestriction/Patches/DNAPatch.cs
@@ -1,8 +1,9 @@
+using System.Linq;
 using System.Reflection;
 
 using HarmonyLib;
-
 using Macchacoffee.ElinMods.ModUtility.Patch;
+using UnityEngine;
 
 namespace Macchacoffee.ElinMods.AbilityRestriction.Patches;
 
@@ -29,14 +30,47 @@ internal static class DNAPatch
         for (int i = 0; i < __instance.vals.Count; i += 2)
         {
             var id = __instance.vals[i];
-            // GetInvalidAction()の実装に揃えるため、DNAのアビリティ重複チェック時はIDだけで比較する
-            if (ModContext.WorldConfig.GetDeniedAbility(c.uid)?.ContainsId(id) == true)
+            if (IsRestrectedAbility(c, id) && HasUnderlyingAbility(c, id))
             {
-                // 制限しているアビリティのElementを復元し、
+                // 制限中の習得済みアビリティのElementを復元し、
                 // 遺伝子の注入時のアビリティ重複チェックが正しく機能するようにする
                 __result = Element.Create(id, __instance.vals[i + 1]);
                 return;
             }
         }
+    }
+
+    private static bool IsRestrectedAbility(Chara chara, int actId)
+    {
+        // GetInvalidAction()の実装に揃えるため、DNAのアビリティ重複チェック時はIDだけで比較する
+        return ModContext.WorldConfig.GetDeniedAbility(chara.uid)?.ContainsId(actId) == true;
+    }
+
+    private static bool HasUnderlyingAbility(Chara chara, int actId)
+    {
+        // Charaに設定されたアビリティを確認する。
+        if (chara._listAbility?.Any(x => Mathf.Abs(x) == actId) == true)
+        {
+            return true;
+        }
+
+        // SourceCharaに設定されたアビリティを確認する。
+        foreach (var act in chara.source.actCombat)
+        {
+            var alias = act.Split('/')[0];
+
+            if (chara.MainElement != Element.Void
+                && EClass.sources.elements.alias[alias].aliasRef == "mold")
+            {
+                alias += chara.MainElement.source.alias.Replace("ele", "");
+            }
+
+            if (ACT.dict[alias].source.id == actId)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
